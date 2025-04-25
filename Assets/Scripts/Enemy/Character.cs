2ffc37f9a3228de6f338e2a2ecab52682894fa85
MuraTomo0115 <murata.tomoya2005@gmail.com>
2025-04-25 14:17:14 +0900

    プレイヤーと敵がダメージを受ける処理を作成中

--- Assets/Scripts/Enemy/Character.cs
+++ Assets/Scripts/Enemy/Character.cs
@@ -7,5 +7,39 @@ public class Character : MonoBehaviour
     [SerializeField]
     private string _characterId;
 
-    public string GetCharaId() => _characterId;
+    private int _maxHealth;
+    private int _currentHealth;
+    private int _attackPower;
+
+    public string CharacterId => _characterId;
+    public int AttackPower => _attackPower;
+    public int CurrentHealth => _currentHealth;
+    public int MaxHealth => _maxHealth;
+
+    // CharacterManagerから渡される
+    public void SetStats(int maxHp, int atk)
+    {
+        _maxHealth = maxHp;
+        _attackPower = atk;
+        _currentHealth = maxHp;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+
+        Debug.Log($"{_characterId} took {damage} damage! HP: {_currentHealth}");
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{_characterId} died.");
+        Destroy(gameObject); // ゲームオブジェクト削除
+    }
 }
