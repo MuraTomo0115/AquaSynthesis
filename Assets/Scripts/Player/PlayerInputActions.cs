a997c5075e66fad68bfd9ded61ae198d48d77e33
MuraTomo0115 <murata.tomoya2005@gmail.com>
2025-05-07 15:08:46 +0900

    プレイヤー仮素材導入・攻撃処理作成中

--- Assets/Scripts/Player/PlayerInputActions.cs
+++ Assets/Scripts/Player/PlayerInputActions.cs
@@ -33,6 +33,14 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""2ba7bb24-9e72-4be1-a1bc-dfe8876096f1"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -123,6 +131,28 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""27a18e57-5c62-41f7-994b-a7c0f51c6e62"",
+                    ""path"": ""<Keyboard>/enter"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4ddbe945-b464-41f4-b06c-beff1eb4221d"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -133,6 +163,7 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -184,12 +215,14 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Attack;
     public struct PlayerActions
     {
         private @PlayerInputActions m_Wrapper;
         public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Attack => m_Wrapper.m_Player_Attack;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -205,6 +238,9 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Attack.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -215,6 +251,9 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
             }
         }
     }
@@ -223,5 +262,6 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _moveSpeed = 5f;  // 移動速度
+    private float     _moveSpeed = 5f;  // 移動速度
     [SerializeField]
-    private float _jumpForce = 5f;  // ジャンプ力
+    private float     _jumpForce = 5f;  // ジャンプ力
     [SerializeField]
-    private float _ray = 1f;        // 地面を検出するレイの長さ
+    private float     _ray = 1f;        // 地面を検出するレイの長さ
     [SerializeField]
     private Transform _groundCheck; // 足元の空オブジェクト
     [SerializeField]
-    private LayerMask _groundLayer; // 地面のタグ
-    private Rigidbody2D _rb;
-    private Vector2 _movement;      // 自機
-    private PlayerInputActions _inputActions;
-    private bool _isCanJump = true;
+    private           LayerMask _groundLayer; // 地面のタグ
+    private           Rigidbody2D _rb;
+    private           Vector2 _movement;      // 自機
+    private           PlayerInputActions _inputActions;
+    private bool      _isCanJump = true;
+    private Animator  _animator;
+    private SpriteRenderer _spriteRenderer;
+    private bool _canAttack = true;
+    [SerializeField]
+    private GameObject attackSensor;
 
     private void Awake()
     {
@@ -36,11 +41,15 @@ public class PlayerMovement : MonoBehaviour
         _inputActions.Player.Move.canceled += ctx => _movement = Vector2.zero;
         // ジャンプする
         _inputActions.Player.Jump.performed += ctx => Jump();
+        // 攻撃
+        _inputActions.Player.Attack.performed += ctx => Attack();
     }
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     /// <summary>
@@ -50,15 +59,40 @@ public class PlayerMovement : MonoBehaviour
     {
         _rb.velocity = new Vector2(_movement.x * _moveSpeed, _rb.velocity.y);
 
-        //地面チェック
+        if (Mathf.Abs(_movement.x) > Mathf.Epsilon)
+        {
+            _animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            _animator.SetInteger("AnimState", 0);
+        }
+
+        _animator.SetFloat("FallSpeed", _rb.velocity.y);
+
+        // 移動方向に応じて向きを変える
+        if (_movement.x > 0.01f)
+        {
+            _spriteRenderer.flipX = false; // 右向き
+            attackSensor.transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+        else if (_movement.x < -0.01f)
+        {
+            _spriteRenderer.flipX = true; // 左向き
+            attackSensor.transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+        }
+
+        // 地面チェック
         RaycastHit2D hit = Physics2D.Raycast(_groundCheck.position, Vector2.down, _ray, _groundLayer);
         if (hit.collider != null && hit.collider.CompareTag("Ground"))
         {
             _isCanJump = true;
+            _animator.SetBool("isGround", true);
         }
         else
         {
-            _isCanJump= false;
+            _isCanJump = false;
+            _animator.SetBool("isGround", false);
         }
     }
 
@@ -68,11 +102,40 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         // フラグが無効の場合ジャンプしない
-        if (!_isCanJump)
-            return;
+        if (!_isCanJump) return;
 
         // ForceMode2Dを使用し、瞬発的にジャンプ
+        _animator.SetTrigger("Jump");
         _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         _isCanJump = false;
+    }
+
+    private void Attack()
+    {
+        if (!_canAttack) return; // 攻撃が許可されていなければ中断
+
+        _animator.SetTrigger("AttackSword");
+    }
+
+    public void trueAttack()
+    {
+        _canAttack = true;
+        _animator.SetBool("isAttack", true );
+    }
+
+    public void falseAttack()
+    {
+        _canAttack = false;
+        _animator.SetBool("isAttack", false);
+    }
+
+    public void StartAttack()
+    {
+        attackSensor.gameObject.SetActive(true);
+    }
+
+    public void EndAttack()
+    {
+        attackSensor.gameObject.SetActive(false);
     }
 }
